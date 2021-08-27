30a1c7b64bcce743e102bdd80f10f7c59ee0badb
A. Y. Firdaus <amyar32@gmail.com>
2021-08-27 15:48:33 +0700

    add author information in article

--- src/views/Articles/ArticleDetail.vue
+++ src/views/Articles/ArticleDetail.vue
@@ -1,9 +1,9 @@
 <template>
   <loading-screen v-if="isLoading" />
 
-  <div v-else class="flex justify-center">
+  <div v-else class="flex justify-center space-x-2">
     <!-- left -->
-    <div class="hidden xl:w-4/12"></div>
+    <div class="hidden"></div>
 
     <!-- center -->
     <base-card class="w-11/12 md:w-7/12 xl:w-6/12 space-y-3 p-9">
@@ -33,7 +33,42 @@
     ></base-card>
 
     <!-- right -->
-    <div class="hidden xl:w-4/12"></div>
+    <div class="hidden md:flex md:w-4/12 xl:w-3/12">
+      <div class="w-full">
+        <base-card
+          class="flex justify-center flex-col md:w-11/12 space-y-2 py-10 px-5"
+        >
+          <div class="flex justify-center">
+            <img
+              :src="baseUrl + article.author.picture.formats.small.url"
+              alt="article.author.name"
+              class="w-40 rounded-full"
+            />
+          </div>
+          <div class="text-center">
+            <h2 class="text-2xl font-bold">{{ article.author.name }}</h2>
+            <p class="text-sm italic text-gray-500">
+              {{ article.author.email }}
+            </p>
+          </div>
+          <div class="divider"></div>
+          <div class="text-center space-y-3">
+            <div>
+              <h3 class="text-xs font-bold text-gray-500">BIO</h3>
+              <p class="text-sm">{{ article.author.bio }}</p>
+            </div>
+            <div>
+              <h3 class="text-xs font-bold text-gray-500">JABATAN</h3>
+              <p class="text-sm">{{ article.author.position }}</p>
+            </div>
+            <div>
+              <h3 class="text-xs font-bold text-gray-500">LOKASI</h3>
+              <p class="text-sm">{{ article.author.location }}</p>
+            </div>
+          </div>
+        </base-card>
+      </div>
+    </div>
   </div>
 </template>
 
