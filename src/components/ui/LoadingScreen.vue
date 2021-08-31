df1f4b88bad4eebf9f8b75098d48e7e6214338f3
A. Y. Firdaus <amyar32@gmail.com>
2021-08-31 15:59:49 +0700

    add props to loading screen comp

--- src/components/ui/LoadingScreen.vue
+++ src/components/ui/LoadingScreen.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="flex justify-center items-center min-h-screen">
-    <button class="btn btn-lg loading">loading</button>
+    <button class="btn btn-lg loading">{{ errorMessage }}</button>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["error"],
+
+  computed: {
+    errorMessage() {
+      return !this.error ? "Loading" : "Terjadi Sesuatu Kesalahan";
+    },
+  },
+};
+</script>
