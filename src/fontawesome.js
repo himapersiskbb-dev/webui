49e7e3f7c614a92c2b5abdb7ea3950a721a4015a
A. Y. Firdaus <amyar32@gmail.com>
2021-08-31 16:30:00 +0700

    remove unused icon

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,26 +2,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
   faBars,
-  faSignInAlt,
   faFileAlt,
   faFileDownload,
   faIdBadge,
-  faSignOutAlt,
-  faUserShield,
   faTimes,
-  faCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(
-  faBars,
-  faSignInAlt,
-  faFileAlt,
-  faFileDownload,
-  faIdBadge,
-  faSignOutAlt,
-  faUserShield,
-  faTimes,
-  faCaretDown
-);
+library.add(faBars, faFileAlt, faFileDownload, faIdBadge, faTimes);
 
 export default FontAwesomeIcon;
